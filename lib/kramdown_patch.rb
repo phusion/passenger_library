f335d8c29dff5fa8ce82616af8acfe06a20f7322
Hongli Lai (Phusion) <hongli@phusion.nl>
2015-07-03 14:28:01 +0200

    Improve Kramdown header ID generator
    - Correctly strip away non-alphabetic characters like '"' and '?'
    - Remove number prefixes

--- lib/kramdown_patch.rb
+++ lib/kramdown_patch.rb
@@ -1,12 +1,13 @@
-# A monkeypatch for the Kramdown Markdown renderer. Improves automatic header ID generation by preserving underscores.
+# A monkeypatch for the Kramdown Markdown renderer. Improves automatic header
+# ID generation by preserving underscores and by removing number prefixes.
 Kramdown::Converter::Base.class_eval do
   def generate_id(str)
     str = ::Kramdown::Utils::Unidecoder.decode(str) if @options[:transliterated_header_ids]
-    gen_id = str.gsub(/^[^a-zA-Z]+/, '')
-    gen_id.tr!('^a-zA-Z0-9 -_', '')
-    gen_id.tr!(' ', '-')
+    gen_id = str.sub(/^[0-9]+/, '') # Remove number prefixes
+    gen_id.strip!
+    gen_id = gen_id.scan(/[a-z0-9\-_]+/i).join("-")
     gen_id.downcase!
-    gen_id = 'section' if gen_id.length == 0
+    gen_id = 'section' if gen_id.empty?
     @used_ids ||= {}
     if @used_ids.has_key?(gen_id)
       gen_id += '-' << (@used_ids[gen_id] += 1).to_s
