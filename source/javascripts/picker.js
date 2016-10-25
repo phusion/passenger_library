883f3a1b4f2f13e6d575e11722b546b5a8d3280e
Camden Narzt <c.narzt@me.com>
2016-10-25 11:20:50 -0600

    Specify node 4 is needed for meteor 1.4

--- source/javascripts/picker.js
+++ source/javascripts/picker.js
@@ -4,3 +4,10 @@ function proxy_picker_callback(){
 }
 $('#proxy_picker').change(proxy_picker_callback);
 $('#proxy_picker').change();
+
+function meteor_picker_callback(){
+  $("div[role='main'] .meteor").hide();
+  $("."+$('#meteor_picker').val().toLowerCase()).show();
+}
+$('#meteor_picker').change(meteor_picker_callback);
+$('#meteor_picker').change();
