82898f54329637feea0c8e944a0a78b0f638f434
Niels van der Zanden <niels@phusion.nl>
2017-09-02 20:37:09 +0200

    Add we are hiring banner to each page.

--- config.rb
+++ config.rb
@@ -462,12 +462,14 @@ activate :relative_assets
 configure :development do
   set :url_root, DEVELOPMENT_URL_ROOT
   activate :livereload, :port => 35730
+  set :hiring_banner, true
 end
 
 # Build-specific configuration
 configure :build do
   set :url_root, PRODUCTION_URL_ROOT
   set :google_analytics, true
+  set :hiring_banner, true
 
   # For example, change the Compass output style for deployment
   activate :minify_css
