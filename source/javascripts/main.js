6862884f8021b80119e1fb0ae69465701d026f51
Luuk <luukhafkamp@gmail.com>
2018-09-13 17:45:12 +0200

    Changed the copy button, removed bootstrap/jquery

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -4,7 +4,7 @@ window.makeDropdown = makeDropdown;
 import redirect_to_lang from './redirect_to_lang';
 window.redirect_to_lang = redirect_to_lang;
 
-import './algolia-docsearch'
+import './algolia-docsearch';
 import './automenu';
 import './copy_buttons';
 import './install_os_selection';
