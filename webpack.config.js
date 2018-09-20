706daaa5578a703056221b94ac170edff170d6f1
Luuk <luukhafkamp@gmail.com>
2018-09-20 15:01:49 +0200

    Upgraded/cleaned up webpack + added autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,43 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  entry: {
-    bundle: ['./source/javascripts/main.js', './source/stylesheets/main.scss']
-  },
-
+  mode: 'none',
+  entry: ['./source/javascripts/main.js', './source/stylesheets/main.scss'],
   output: {
-    filename: '[name].js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'tmp/webpack')
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules|\.tmp|vendor/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015']
         }
       },
-
       {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          use: 'css-loader?importLoaders=1',
-        }),
-      },
-
-      {
-        test: /\.(sass|scss)$/,
-        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()]
+            }
+          },
+          'sass-loader'
+        ]
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin({
+    new MiniCssExtractPlugin({
       filename: 'bundle.css',
-      allChunks: true,
-    }),
-  ],
+    })
+  ]
 };
