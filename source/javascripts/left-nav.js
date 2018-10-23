87105fba26154746ddf72bf2e0153423295ab834
Luuk <luukhafkamp@gmail.com>
2018-10-23 16:45:48 +0200

    Adjusted navigation repositioning

--- source/javascripts/left-nav.js
+++ source/javascripts/left-nav.js
@@ -2,6 +2,7 @@ const mobileBtn = document.querySelector('#nav-btn');
 const leftNav = document.querySelector('#left-nav');
 const footer = document.querySelector('footer');
 const left = document.querySelector('#left-nav-content');
+const rightToC = document.querySelector('#right-content-toc');
 let last_known_scroll_position = 0;
 let ticking = false;
 
@@ -19,19 +20,27 @@ function showMenu() {
   }
 }
 
+function repositionNav(element) {
+  if (window.scrollY > 30 && window.innerHeight > 680) {
+    element.classList.add('repos-nav');
+  } else {
+    element.classList.remove('repos-nav');
+  }
+}
+
 function navScroll() {
   let leftRect = left.getBoundingClientRect();
   let footerRect = footer.getBoundingClientRect();
-  if (footerRect.y - leftRect.y - leftRect.height <= 120 && leftRect.y <= 100) {
+  if (footerRect.y - leftRect.y - leftRect.height <= 120 && leftRect.y <= 30) {
     left.classList.add('stop-nav');
-  } else if (leftRect.y >= 100) {
+  } else if (leftRect.y >= 30) {
     left.classList.remove('stop-nav');
   }
 
-  if (window.scrollY > 30 && window.innerHeight > 680) {
-    left.classList.add('repos-nav');
-  } else {
-    left.classList.remove('repos-nav');
+  repositionNav(left);
+
+  if (rightToC) {
+    repositionNav(rightToC);
   }
 }
 
