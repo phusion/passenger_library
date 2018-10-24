2d32cae3f8a993e96d9064dd84f8a557913e483d
Niels van der Zanden <niels@phusion.nl>
2018-10-24 13:04:53 +0200

    Do not force hide mobile menu on close

--- source/javascripts/left-nav.js
+++ source/javascripts/left-nav.js
@@ -9,11 +9,9 @@ let ticking = false;
 function showMenu() {
   if (!leftNav.classList.contains('show')) {
     leftNav.classList.add('show');
-    leftNav.classList.remove('hidden');
     mobileBtn.classList.add('close-menu');
     mobileBtn.classList.remove('open-menu');
   } else {
-    leftNav.classList.add('hidden');
     leftNav.classList.remove('show');
     mobileBtn.classList.add('open-menu');
     mobileBtn.classList.remove('close-menu');
