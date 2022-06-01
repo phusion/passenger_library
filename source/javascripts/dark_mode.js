dbf16607f6398f429aac4e58bd4cf02439138e7d
Niels van der Zanden <niels@pharynx.nl>
2022-06-01 09:40:26 +0200

    Fix dark mode toggle on OS'es with dark mode as preferred mode

--- source/javascripts/dark_mode.js
+++ source/javascripts/dark_mode.js
@@ -1,10 +1,10 @@
 class Mode {
-  constructor(container, screenMode, localStorage) {
-    this.body = document.querySelector('body')
-    this.container = container
-    this.mode = screenMode
-    this.storage = localStorage
-    this.renderInput()
+  constructor(container, screenModes, localStorage) {
+    this.body = document.querySelector('body');
+    this.container = container;
+    this.modes = screenModes;
+    this.storage = localStorage;
+    this.renderInput();
   }
 
   renderInput() {
@@ -14,43 +14,46 @@ class Mode {
           <input type="checkbox">
           <span class="slider round"></span>
         </label>
-      `)
+      `);
 
-      const input = document.querySelector('.switch input')
+      const input = document.querySelector('.switch input');
 
-      this.eventHandler(input)
-      this.checkStorage(input)
+      this.eventHandler(input);
+      this.checkStorage(input);
     }
   }
 
   eventHandler(input) {
-    input.addEventListener('change', this.changeMode.bind(this, input))
+    input.addEventListener('change', this.changeMode.bind(this, input));
   }
 
   changeMode(input) {
-    if (input.checked) {
-      this.body.classList.add(this.mode)
-      this.storeData(this.mode)
-    } else if (this.body.classList.contains(this.mode)) {
-      this.body.classList.remove(this.mode)
-      this.storeData('')
+    let mode = this.modes[input.checked];
+    if(!this.body.classList.contains(mode)) {
+      this.body.classList.toggle(this.modes[!input.checked]);
+      this.body.classList.toggle(this.modes[input.checked]);
+      this.storeData(mode);
     }
   }
 
   storeData(mode) {
-    localStorage.setItem('screen_mode', mode)
+    localStorage.setItem('screen_mode', mode);
   }
 
   checkStorage(input) {
-    if (this.storage) {
-      this.body.classList.add(this.mode)
-      input.checked = true
+    if(Object.values(this.modes).includes(this.storage)) {
+      this.body.classList.add(this.storage);
+      input.checked = this.storage == this.modes[true];
+    } else {
+      let dark = !!(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)'));
+      this.body.classList.add(this.modes[dark]);
+      input.checked = dark;
     }
   }
 }
 
 new Mode(
   document.querySelector('#screen-mode img'),
-  'dark-mode',
+  { true: 'dark-mode', false: 'light-mode' },
   localStorage.getItem('screen_mode')
-)
+);
